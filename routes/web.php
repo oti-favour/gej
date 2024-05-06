b5a902e6d527b9fefdc80849051fa3453825d265
oti-favour <favourgraceoti@gmail.com>
2024-05-06 11:28:11 +0100

    gallery

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::get('/services', [CustomAuthController::class, 'services'])->name('servic
 Route::get('/contact-us', [CustomAuthController::class, 'contact'])->name('contact');
 Route::get('/waef', [CustomAuthController::class, 'waef'])->name('waef');
 Route::get('/gallery', [CustomAuthController::class, 'gallery'])->name('gallery');
+Route::get('/blogpost', [CustomAuthController::class, 'blogpost'])->name('blogpost');
 Route::post('/contact-us/submit', [CustomAuthController::class, 'submitContactForm'])->name('contact.submit');
 Route::get('/admin/login', [CustomAuthController::class, 'adminLogin'])->name('admin.login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
