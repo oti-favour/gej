7f39a332a773b6e00efc09057698fda54db255c0
oti-favour <favourgraceoti@gmail.com>
2024-05-06 09:59:14 +0100

    gallery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,1 @@
 import './bootstrap';
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
