59dcc10d6b120e6fc83c35d8853db3186306b9e9
oti-favour <favourgraceoti@gmail.com>
2024-05-06 16:20:46 +0100

    gallery

--- resources/views/waef.blade.php
+++ resources/views/waef.blade.php
@@ -166,47 +166,15 @@
             </div>
         </div>
 
-        <div class="container-xxl bg-light my-5 py-5">
-            <div class="container py-5">
+        <div class="container-xxl bg-light my-3 py-2">
+            <div class="container py-3">
                 <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
                     <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Latest News</div>
                     
                 </div>
                 <div class="row g-4 justify-content-center">
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
-                            <div class="text-center p-4 pt-0">
-                                <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
-                                    <small>Political Analysis </small>
-                                </div>
-                                <h5 class="mb-3">Coup d’états: Dangers of democratic disruption and African solutions</h5>
-                                <p>Coup d’états: Dangers of democratic disruption and African solutions A news analysis by Emmanuel Oloniruha, News Agency of Nigeria (NAN)…</p>
-                                {{-- <div class="causes-progress bg-light p-3 pt-2">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="text-dark">$10,000 <small class="text-body">Goal</small></p>
-                                        <p class="text-dark">$9,542 <small class="text-body">Raised</small></p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                                            <span>90%</span>
-                                        </div>
-                                    </div>
-                                </div> --}}
-                            </div>
-                            <div class="position-relative mt-auto">
-                                <img class="img-fluid" src="{{asset('img/courses-1.jpg')}}" alt="">
-                                <div class="causes-overlay">
-                                    <a class="btn btn-outline-primary" href="">
-                                        Read More
-                                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
-                                            <i class="fa fa-arrow-right"></i>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
                             <div class="text-center p-4 pt-0">
                                 <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
@@ -227,7 +195,7 @@
                                 </div> --}}
                             </div>
                             <div class="position-relative mt-auto">
-                                <img class="img-fluid" src="{{asset('img/courses-2.jpg')}}" alt="">
+                                <img class="img-fluid w-100" src="{{asset('img/courses-2.jpg')}}" alt="">
                                 <div class="causes-overlay">
                                     <a class="btn btn-outline-primary" href="">
                                         Read More
@@ -239,7 +207,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                         <div class="causes-item d-flex flex-column bg-white border-top border-5 border-primary rounded-top overflow-hidden h-100">
                             <div class="text-center p-4 pt-0">
                                 <div class="d-inline-block bg-primary text-white rounded-bottom fs-5 pb-1 px-3 mb-4">
@@ -260,7 +228,7 @@
                                 </div> --}}
                             </div>
                             <div class="position-relative mt-auto">
-                                <img class="img-fluid" src="{{asset('img/courses-3.jpg')}}" alt="">
+                                <img class="img-fluid w-100" src="{{asset('img/courses-3.jpg')}}" alt="">
                                 <div class="causes-overlay">
                                     <a class="btn btn-outline-primary" href="">
                                         Read More
@@ -275,8 +243,6 @@
                 </div>
             </div>
         </div>
-
-        
 
     </body>
 @endsection
